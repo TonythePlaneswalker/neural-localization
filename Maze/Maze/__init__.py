70fcd23ea656561fcc743f8613b3c86efa41a2e1
TonythePlaneswalker <tonyuanac@gmail.com>
2018-04-10 15:41:46 -0400

    Init file for v1 environment

--- Maze/Maze/__init__.py
+++ Maze/Maze/__init__.py
@@ -1,5 +1,11 @@
 from gym.envs.registration import register
+
 register(
     id='Maze-v0',
     entry_point='Maze.envs.Maze:maze'
+)
+
+register(
+    id='Maze-v1',
+    entry_point='Maze.envs.maze2:maze'
 )
